20a5dbe8b39961dcafc428808c78f2424a081997
Gabriel Vale <gabriel.vale@indicium.tech>
2024-10-28 05:00:44 +0000

    Finalizado mias alguns ajustes necessarios

--- models/staging/Raw_Adventure_Works/stg_bd__salesorderheader.sql
+++ models/staging/Raw_Adventure_Works/stg_bd__salesorderheader.sql
@@ -11,6 +11,7 @@ with
             , cast(SALESPERSONID as int) as fk_sales_person
             , cast(TERRITORYID as int) as fk_territory
             , cast(CREDITCARDID as int) as fk_credit_card
+            , cast(SHIPTOADDRESSID as varchar) as fk_ship_address
             , cast(ORDERDATE as date) as order_date_sales_order
             , cast(STATUS as int) as status_sales_order
             , {{ round_decimals('SUBTOTAL') }} as subtotal_sales_order
@@ -19,16 +20,15 @@ with
             , {{ round_decimals('FREIGHT') }} as freight_sales_order
             , cast(MODIFIEDDATE as date) as modifieddate
             , current_date as transformeddate
-            --, cast(SHIPDATE as date) as ship_date_sales_order
-            --, cast(DUEDATE as date) as due_date_sales_order
-            --, cast(REVISIONNUMBER as int) as revision_number
-            --, cast(PURCHASEORDERNUMBER as varchar) as purchase_order_number
+            --, cast(SHIPDATE as date) as 
+            --, cast(DUEDATE as date) as 
+            --, cast(REVISIONNUMBER as int) as 
+            --, cast(PURCHASEORDERNUMBER as varchar) as 
             --, cast(COMMENT as varchar) as
             --, cast(ROWGUID as varchar) as
             --, cast(CREDITCARDAPPROVALCODE as varchar) as
             --, cast(CURRENCYRATEID as varchar) as
             --, cast(BILLTOADDRESSID as varchar) as
-            --, cast(SHIPTOADDRESSID as varchar) as
             --, cast(SHIPMETHODID as varchar) as            
             --, cast(ACCOUNTNUMBER as varchar) as
             --, cast(ONLINEORDERFLAG as varchar) as
--- tests/test_total_sales_2011.sql
+++ tests/test_total_sales_2011.sql
@@ -7,7 +7,7 @@
 with
     sales_2011 as (
         select sum(total_price) as gross_sale
-        from {{ ref('int_sales_order') }}
+        from {{ ref('int_order_detail') }}
         where order_date_sales_order between '2011-01-01' and '2011-12-31'
     )
 
