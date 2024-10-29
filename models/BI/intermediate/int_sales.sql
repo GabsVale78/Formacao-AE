420ca0dd83b6187da040c374bf5e52d70ec7174a
Gabriel Vale <gabriel.vale@indicium.tech>
2024-10-29 09:42:49 +0000

    Finaliazada manutenção definitiva

--- models/BI/intermediate/int_sales.sql
+++ models/BI/intermediate/int_sales.sql
@@ -1,23 +1,18 @@
 with
 
-    sales_order_reason as (
-        select *
-        from {{ ref('stg_bd__salesorderheadersalesreason') }}
-    )
-    
-    , sales_reason as (
-        select *
-        from {{ ref('stg_bd__salesreason') }}
-    )
-
-    , sales_order as (
+    sales_order as (
         select *
         from {{ ref('stg_bd__salesorderheader') }}
     )
 
-    , int_order_detail as (
+    , order_detail as (
         select *
-        from {{ ref('int_order_detail') }}
+        from {{ ref('stg_bd__salesorderdetail') }}
+    )
+    
+    , product as (
+        select *
+        from {{ ref('stg_bd__product') }}
     )
 
     , status as (
@@ -25,34 +20,20 @@ with
         from {{ ref('status') }}
     )
 
-    , joined1 as (
-        select
-            sales_order.pk_sales_order
-            , sales_reason.REASON_TYPE
-        from sales_order
-        left join sales_order_reason
-            on sales_order.pk_sales_order = sales_order_reason.fk_sales_order
-        left join sales_reason
-            on sales_order_reason.fk_sales_reason = sales_reason.pk_sales_reason
-
-    )
 
     , joined as (
         select
-            int_order_detail.PK_ORDER_DETAIL
+            order_detail.PK_ORDER_DETAIL
             , sales_order.PK_SALES_ORDER as FK_SALES_ORDER
-            , int_order_detail.FK_PRODUCT
+            , order_detail.FK_PRODUCT
             , sales_order.FK_CUSTOMER
-            , sales_order.FK_SALES_PERSON
             , sales_order.FK_TERRITORY
             , sales_order.FK_SHIP_ADDRESS
             , sales_order.FK_CREDIT_CARD
-            , joined1.REASON_TYPE
             , sales_order.ORDER_DATE_SALES_ORDER
-            , int_order_detail.ORDER_QTY
-            , int_order_detail.UNIT_PRICE
-            , int_order_detail.DISCOUNT
-            , int_order_detail.TOTAL_PRICE
+            , order_detail.ORDER_QTY
+            , order_detail.UNIT_PRICE
+            , order_detail.DISCOUNT
             , sales_order.SUBTOTAL_SALES_ORDER
             , sales_order.TAXAMT_SALES_ORDER
             , sales_order.TOTALDUE_SALES_ORDER
@@ -60,11 +41,11 @@ with
             , status.NAME_STATUS
             , sales_order.MODIFIEDDATE
             , sales_order.TRANSFORMEDDATE
-        from int_order_detail
+        from order_detail
         left join sales_order 
-            on int_order_detail.FK_SALES_ORDER = sales_order.PK_SALES_ORDER
-        left join joined1
-            on sales_order.pk_sales_order = joined1.pk_sales_order
+            on order_detail.FK_SALES_ORDER = sales_order.PK_SALES_ORDER
+        left join product
+            on order_detail.fk_product = product.pk_product
         left join status 
             on sales_order.status_sales_order = status.status
     )
@@ -72,6 +53,7 @@ with
     , measure as (
         select
             *
+            , {{ round_decimals('order_qty * unit_price') }}  as total_price
             , round(FREIGHT_SALES_ORDER / NULLIF(count(*) over(partition by FK_SALES_ORDER), 0), 2) as PRORATED_FREIGHT
             , round(TAXAMT_SALES_ORDER / NULLIF(count(*) over(partition by FK_SALES_ORDER), 0), 2) as PRORATED_TAXAMT
             , ORDER_QTY * UNIT_PRICE - (1 - DISCOUNT) as NET_SALES
@@ -89,7 +71,6 @@ with
             , FK_SALES_ORDER
             , FK_PRODUCT
             , FK_CUSTOMER
-            , FK_SALES_PERSON
             , FK_SHIP_ADDRESS
             , FK_TERRITORY
             , FK_CREDIT_CARD
@@ -104,7 +85,6 @@ with
             , TOTALDUE_SALES_ORDER
             , NET_SALES
             , IS_DISCOUNTED
-            , REASON_TYPE
             , NAME_STATUS
             , MODIFIEDDATE
             , TRANSFORMEDDATE  
--- models/BI/marts/dim_purchase_detail.sql
+++
@@ -1,8 +0,0 @@
-with
-    int_purchase_detail as (
-        select *
-        from {{ ref('int_purchase_detail') }}
-    )
-
-select *
-from int_purchase_detail
